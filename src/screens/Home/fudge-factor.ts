95136046386aba6a23867bf2b815270312404edc
Kevin Tjiam <kevin@kevincharm.com>
2020-12-08 21:08:13 +0100

    use geomag for magnetic declination

--- src/screens/Home/fudge-factor.ts
+++ src/screens/Home/fudge-factor.ts
@@ -1,13 +1,4 @@
-import { format } from 'date-fns'
 import { HNL_CENTRE } from './HNL_CENTRE'
-import WorldMagneticModel from './WorldMagneticModel'
+import * as geomag from 'geomag'
 
-export const hnlMagneticDeclination = new WorldMagneticModel().declination(
-    0,
-    HNL_CENTRE[0],
-    HNL_CENTRE[1],
-    Math.max(
-        2010,
-        Math.min(2015, Number(format(new Date(), 'yyyy')))
-    ) /** Only defined for year \in [2010, 2015] */
-)
+export const hnlMagneticDeclination = geomag.field(HNL_CENTRE[0], HNL_CENTRE[1], 0).declination
